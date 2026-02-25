0a42f1e6b9bd896aebce6dfd8fbcc25c1d61e247
vovaDev <146870016+VolodymyrDziu@users.noreply.github.com>
2026-02-25 14:04:15 +0200

    Implement movie list view with add/delete functionality

--- Friends/MovieListView.swift
+++ Friends/MovieListView.swift
@@ -6,13 +6,77 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MovieListView: View {
+    
+    @Query private var movies: [Movie]
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var newMovie: Movie?
+    
+    init( titleFilter: String = "" ){
+        let predicate = #Predicate<Movie> { movie in
+            titleFilter.isEmpty || movie.title.localizedStandardContains(titleFilter)
+        }
+        
+        _movies = Query(filter: predicate, sort: \Movie.title)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        Group {
+            if !movies.isEmpty {
+                List {
+                    ForEach(movies){movie in
+                        NavigationLink(movie.title) {
+                            MovieDetailView(movie: movie, isNew: true)
+                        }
+                        
+                    }
+                    .onDelete(perform: deleteMovie(indexes: ))
+                }
+            } else {
+                ContentUnavailableView("Add movies", systemImage: "film.stack")
+            }
+        }
+            .navigationTitle("Movies")
+            .toolbar {
+                ToolbarItem {
+                    Button("Add", systemImage: "plus", action: addMovie)
+                }
+                ToolbarItem {
+                    EditButton()
+                }
+            }
+            .sheet(item: $newMovie) { movie in
+                NavigationStack {
+                    MovieDetailView(movie: movie, isNew: true)
+                }
+                .interactiveDismissDisabled()
+            }
+        
+    }
+    
+    private func addMovie(){
+        let newMovie = Movie(title: "", releaseDate: .now)
+        modelContext.insert(newMovie)
+        self.newMovie = newMovie
+        
+    }
+    
+    private func deleteMovie(indexes: IndexSet){
+        withAnimation {
+            for index in indexes {
+                modelContext.delete(movies[index])
+            }
+        }
     }
 }
 
 #Preview {
-    MovieListView()
+    NavigationStack {
+        MovieListView()
+            .modelContainer(SampleData.shared.modelContainer)
+    }
 }
