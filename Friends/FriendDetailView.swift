45ae428513acf01036e31363abd1ae279f7ad093
vovaDev <146870016+VolodymyrDziu@users.noreply.github.com>
2026-02-25 14:00:23 +0200

    Enhance FriendDetailView with form fields and actions

--- Friends/FriendDetailView.swift
+++ Friends/FriendDetailView.swift
@@ -6,13 +6,59 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FriendDetailView: View {
+    @Bindable var friend: Friend
+    let isNew: Bool
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \Movie.title) private var movies: [Movie]
+    
+    init(friend: Friend, isNew: Bool) {
+        self.friend = friend
+        self.isNew = isNew
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Name", text: $friend.name)
+                .autocorrectionDisabled()
+            
+            Picker("favoriteMovie", selection: $friend.favoritMovie){
+                Text("None")
+                    .tag(nil as Movie?)
+                
+                ForEach(movies){ movie in
+                    Text(movie.title)
+                        .tag(movie)
+                }
+            }
+        }
+        .navigationTitle(isNew ? "New friend" : "Friend")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            if isNew {
+                ToolbarItem( placement: .confirmationAction) {
+                    Button("Save"){
+                        dismiss()
+                    }
+                }
+                ToolbarItem( placement: .cancellationAction) {
+                    Button("Cancel"){
+                        modelContext.delete(friend)
+                        dismiss()
+                    }
+                }
+            }
+        }  
     }
 }
 
 #Preview {
-    FriendDetailView()
+    NavigationStack {
+        FriendDetailView(friend: SampleData.shared.friend, isNew: true)
+    }
+    .modelContainer(SampleData.shared.modelContainer)
 }
