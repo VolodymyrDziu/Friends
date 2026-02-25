c0e735623f8ea5cae388f0a7ff4eaa9337b24f00
vovaDev <146870016+VolodymyrDziu@users.noreply.github.com>
2026-02-25 14:03:32 +0200

    Implement movie detail view with editing capabilities
    Added functionality to edit movie details and manage favorites.

--- Friends/MovieDetailView.swift
+++ Friends/MovieDetailView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct MovieDetailView: View {
+    @Bindable var movie: Movie
+    let isNew: Bool
+    
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
+    init(movie: Movie, isNew: Bool) {
+        self.movie = movie
+        self.isNew = isNew
+    }
+    
+    var sortedFriends: [Friend] {
+        movie.fovoritBy.sorted {first, second in
+            first.name < second.name
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            TextField("Title", text: $movie.title)
+               
+            DatePicker("Relise Date", selection: $movie.releaseDate, displayedComponents: .date)
+            
+            if !movie.fovoritBy.isEmpty {
+                Section("Favorite by"){
+                    ForEach(sortedFriends) { friend in
+                        Text(friend.name)
+                    }
+                    
+                }
+            }
+        }
+        .navigationTitle( isNew ? "New movie" : "Movie")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            if isNew {
+                ToolbarItem (placement: .confirmationAction){
+                    Button("Save"){
+                        dismiss()
+                    }
+                }
+                ToolbarItem (placement: .cancellationAction){
+                    Button("Cancel"){
+                        modelContext.delete(movie)
+                        dismiss()
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    MovieDetailView()
+    NavigationStack {
+        MovieDetailView(movie: SampleData.shared.movie, isNew: true)
+    }
 }
