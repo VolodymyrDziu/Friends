465b8e81ddafa84808e9ea0f02fae91745db32fa
vovaDev <146870016+VolodymyrDziu@users.noreply.github.com>
2026-02-25 14:04:55 +0200

    Add SampleData class with sample data insertion
    Implement SampleData class for managing friends and movies.

--- Friends/SampleData.swift
+++ Friends/SampleData.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import SwiftData
+
+@MainActor
+class SampleData {
+  
+    static let shared = SampleData()
+   
+    let modelContainer: ModelContainer
+   
+    
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    var friend: Friend {
+        Friend.sampleData.first!
+    }
+    
+    var movie: Movie {
+        Movie.sampleData.first!
+    }
+    
+    private init() {
+        let schema = Schema([
+            Friend.self,
+            Movie.self,
+        ])
+        
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            
+            inserSampleData()
+            try context.save()
+            
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+    
+    private func inserSampleData(){
+        for friend in Friend.sampleData {
+            context.insert(friend)
+        }
+        for movie in Movie.sampleData {
+            context.insert(movie)
+        }
+        
+        Friend.sampleData[0].favoritMovie = Movie.sampleData[1]
+        Friend.sampleData[2].favoritMovie = Movie.sampleData[0]
+        Friend.sampleData[3].favoritMovie = Movie.sampleData[4]
+        Friend.sampleData[4].favoritMovie = Movie.sampleData[2]
+    }
+}
